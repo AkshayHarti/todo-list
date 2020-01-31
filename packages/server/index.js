77431ab9675874df8f49f8a6a143425f91878a46
Akshay <akshay@persuit.com>
2020-01-31 17:33:55 +1100

    Set up apollo graphql server and mongoose

---
+++ packages/server/index.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql } = require("apollo-server");
+require("./config");
+const { Todos } = require("./model");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Todos {
+    text: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    getTodos: [Todos]
+  }
+`;
+
+const todos = [
+  { text: "My" },
+  { text: "first" },
+  { text: "todo" },
+  { text: "app" }
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    getTodos: async () => await Todos.find({}).exec()
+  }
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
