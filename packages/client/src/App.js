e962fee0c5415f1e226f72b2f77115ecb1b4dfe7
akshay <akshay.r555@gmail.com>
2020-02-02 23:06:45 +1100

    Hooked in ApolloClient and added getTodos query

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_TODOS = gql`
+	{
+		getTodos {
+			_id
+			text
+		}
+	}
+`
 
 function App() {
-	return <div className="App">Hello world</div>
+	const { loading, error, data } = useQuery(GET_TODOS)
+
+	if (loading) return <p>Loading...</p>
+	if (error) return <p>Error :(</p>
+
+	const todos = data.getTodos
+
+	return (
+		<div className="App">
+			<ul>
+				{todos.map((value, index) => (
+					<li key={index}>{value.text}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default App
