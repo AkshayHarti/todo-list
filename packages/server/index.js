060d963b1d962f1c19afcad431c54bd1528c4115
Akshay <akshay@persuit.com>
2020-04-26 16:29:46 +1000

    test

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,35 +2,7 @@ const { ApolloServer, gql } = require("apollo-server");
 // require("./config");
 const resolvers = require("./resolvers");
 const { Todos } = require("./model");
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Todos {
-    _id: ID
-    text: String
-    timestamp: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-
-  type Query {
-    getTodos: [Todos]
-  }
-
-  type Mutation {
-    addTodo(text: String): Todos
-    updateTodo(_id: ID!, text: String!): Todos
-    deleteTodo(_id: ID!): Boolean
-    moveTodo(oldIndex: Int!, newIndex: Int!): Boolean
-  }
-`;
+import typeDefs from "./schema";
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
---
+++ packages/server/schema.js
@@ -0,0 +1,30 @@
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Todos {
+    _id: ID
+    text: String
+    timestamp: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+
+  type Query {
+    getTodos: [Todos]
+  }
+
+  type Mutation {
+    addTodo(text: String): Todos
+    updateTodo(_id: ID!, text: String!): Todos
+    deleteTodo(_id: ID!): Boolean
+    moveTodo(oldIndex: Int!, newIndex: Int!): Boolean
+  }
+`;
+
+export default typeDefs;
