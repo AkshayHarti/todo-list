d60d5085d73e83e25ebfd0097c5c1d0227fe7d36
Akshay <akshay@persuit.com>
2020-04-27 20:01:08 +1000

    tests

--- packages/server/config.js
+++ packages/server/config.js
@@ -9,10 +9,7 @@ const db = "todo-db";
 let url = "";
 
 if (process.env.NODE_ENV === "production") {
-  url = process.env.MONGO_CONN_STRING;
-
-  //mongodb+srv://akshay@cluster0-dgat9.mongodb.net/admin?replicaSet=Cluster0-shard-0&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=TodoDB-mongo-cloud&3t.databases=admin,todo-db
-  // "mongodb://akshay:akshay123@ds237357.mlab.com:37357/todo-db";
+  url = "mongodb://akshay:akshay123@ds237357.mlab.com:37357/todo-db";
 } else {
   url = `mongodb://${ip}:${port}/${db}`;
 }
