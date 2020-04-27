b5f20a155658a2980f03343c54e9d54db59e49be
Akshay <akshay@persuit.com>
2020-04-27 19:41:53 +1000

    mongo conn string env

--- packages/server/config.js
+++ packages/server/config.js
@@ -9,8 +9,10 @@ const db = "todo-db";
 let url = "";
 
 if (process.env.NODE_ENV === "production") {
-  url = "mongodb://akshay:akshay123@ds237357.mlab.com:37357/todo-db";
-  // "mongodb://ubt40dsdp5m4ehdajxzi:w82yQv7ivLgR4euAqX0J@btg1fklfnkgc30r-mongodb.services.clever-cloud.com:27017/btg1fklfnkgc30r";
+  url = process.env.MONGO_CONN_STRING;
+
+  //mongodb+srv://akshay@cluster0-dgat9.mongodb.net/admin?replicaSet=Cluster0-shard-0&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=TodoDB-mongo-cloud&3t.databases=admin,todo-db
+  // "mongodb://akshay:akshay123@ds237357.mlab.com:37357/todo-db";
 } else {
   url = `mongodb://${ip}:${port}/${db}`;
 }
