07eda3bd98bf3a60f038a0e6d8b3ce16a1b1d71c
Akshay <akshay@persuit.com>
2020-05-02 21:40:43 +1000

    Added a server file to open optimized client

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reactime from "reactime";
 import ApolloClient, { gql } from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import { TodoContextProvider } from "./todo-context";
-import { Query } from "react-apollo";
 import RouteApp from "./routes";
 
 const GET_TODOS = gql`
@@ -16,28 +14,32 @@ const GET_TODOS = gql`
   }
 `;
 
+const uri =
+  process.env.NODE_ENV === "production"
+    ? "https://todo-lean.herokuapp.com/graphql"
+    : "http://localhost:4000/graphql";
+
 const client = new ApolloClient({
-  uri: "https://todo-lean.herokuapp.com/graphql",
+  uri,
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <Query query={GET_TODOS}>
-      {({ error, loading, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error :(</p>;
+const App = () => {
+  const { data, loading, error } = useQuery(GET_TODOS);
 
-        return (
-          <TodoContextProvider state={data.getTodos}>
-            <RouteApp />
-          </TodoContextProvider>
-        );
-      }}
-    </Query>
-  </ApolloProvider>
-);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <TodoContextProvider state={data.getTodos}>
+      <RouteApp />
+    </TodoContextProvider>
+  );
+};
 
 const rootContainer = document.getElementById("root");
-ReactDOM.render(<App />, rootContainer);
-
-reactime(rootContainer);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  rootContainer
+);
