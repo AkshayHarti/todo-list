6354566f474408ab333409c8c03edc86680c5399
Akshay <akshay@persuit.com>
2020-05-03 17:26:03 +1000

    Updates

---
+++ precache-manifest.a71e2a57ba8e11c88c8c8c0bd5fdf31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5783130eec1134a23ee3b4aa6ecc1e7a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "1090f0dc84ec70573099",
+    "url": "/todo-list/static/js/2.1042e9cb.chunk.js"
+  },
+  {
+    "revision": "216ca9b2a14a7e4372559c705dbf1f7b",
+    "url": "/todo-list/static/js/2.1042e9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a365ff08f0364045644",
+    "url": "/todo-list/static/js/main.b5d194a5.chunk.js"
+  },
+  {
+    "revision": "e22581066e7a971e9b48",
+    "url": "/todo-list/static/js/runtime-main.1fa8a5bc.js"
+  }
+]);
